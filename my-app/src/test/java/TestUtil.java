7f8a23c0c21b2d6a0cbcd9608d36b2d76536f945
Aylindogn <aylindogan247@gmail.com>
2024-03-31 14:04:11 +0300

    add test compute tests

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -13,4 +13,38 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+  @Test
+  public void testSingleArgument() {
+    assertFalse(c.compute(5));
+  }
+
+  @Test
+  public void testEvenNumberOfArguments() {
+    assertFalse(c.compute(2, 4));
+  }
+
+  @Test
+  public void testSumDivisibleByArgument() {
+    assertTrue(c.compute(3, 4, 5));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testZeroArgument() {
+    c.compute(3, 0, 5);
+  }
+
+  @Test
+  public void testNoArgument() {
+    assertFalse(c.compute());
+  }
+
+  @Test
+  public void testOddNumberOfArguments() {
+    assertTrue(c.compute(2,4,6));
+  }
+
+  @Test
+  public void testSumNotDivisibleByArgument() {
+    assertFalse(c.compute(3, 5, 6));
+  }
 }
